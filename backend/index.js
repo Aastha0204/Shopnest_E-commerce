7e8491cbb564936503085f7fb103199bfef160f0
Aastha Sharma <sharma02aastha@gmail.com>
2026-06-30 15:34:52 +0530

    Update frontend API URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDB();
 const app = express();
 app.use(cors(
     {
-        origin: ['http://localhost:3000','http://127.0.0.1:3000'],
+        origin: ['https://shopnest-e-commerce-5tq9.onrender.com','http://127.0.0.1:3000'],
        // methods: ['GET','POST','PUT','DELETE'],
         //allowedHeaders: ['Content-Type','Authorization'],
         credentials: true
